6c01868894731670b960b3a564b0732afa16625c
Marko Röper-Grewe <marko.roeper-grewe@extern.brandung.de>
2024-08-19 01:33:23 +0200

    [TASK][V10] CSS improved; EXT Version adopted

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,11 +9,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'PIA / UDG',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
-    'version' => '1.3.0',
+    'version' => '10.0.0',
     'iconIdentifier' => 'ext-randomquote-icon',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.5.99',
+            'typo3' => '10.4.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
