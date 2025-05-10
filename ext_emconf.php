8fcdb2c7558d8f09c4fa94f4fb09c1d07de1ffbb
Marko Röper-Grewe <marko.roeper-grewe@udg.de>
2025-05-10 18:14:28 +0200

    [TASK][v12] v11 test failed. this version is only suitable for v12+

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'iconIdentifier' => 'ext-randomquote-icon',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.99-12.4.99',
+            'typo3' => '12.4.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
