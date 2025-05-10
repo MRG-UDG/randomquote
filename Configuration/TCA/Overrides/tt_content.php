b1de941f34d4c602d01167ad71fd1a1a4fcd4de1
Marko Röper-Grewe <marko.roeper-grewe@udg.de>
2025-05-10 17:10:28 +0200

    [TASK][v12] try to get rid of the incorrect name “m_r_g.randomquote” as a package

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,9 +1,3 @@
 <?php
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'MRG.Randomquote',
-    'Quote',
-    'Random Quote'
-);
-
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('Randomquote', 'Quote', 'Random Quote');
