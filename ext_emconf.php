953608fd658953771ca86a010551a2084f131be5
Marko Röper-Grewe <marko.roeper-grewe@udg.de>
2025-05-10 16:43:17 +0200

    [TASK][v12] version raised

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,11 +8,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'marko.roeper-grewe@udg.de',
     'author_company' => 'PIA / UDG',
     'state' => 'stable',
-    'version' => '1.2.0',
+    'version' => '12.4.0',
     'iconIdentifier' => 'ext-randomquote-icon',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.5.99',
+            'typo3' => '11.5.99-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [
